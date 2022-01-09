cef75263b00fea855b3c01844e9e1aa1e445e8f9
THIENPHUC <THIENPHUC@DESKTOP-M3RI2IA>
2022-01-09 22:05:17 +0700

    add database

---
+++ SQL/proc.sql
@@ -0,0 +1,11 @@
+﻿create proc usp_dangnhap @username nchar(10), @pass nchar(30)
+as
+	if not exists (select * from DocGia where MaDocGia = @username and MatKhau = @pass)
+	begin
+		RAISERROR(N'Mã đọc giả hoặc mật khẩu không đúng',16,1)
+		return
+	end
+	select * from DocGia where MaDocGia = @username and MatKhau = @pass
+go
+
+exec usp_dangnhap N'NL00000001', N'hothienphuc1999'
